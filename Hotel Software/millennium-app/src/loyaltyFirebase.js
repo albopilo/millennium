05522fe46cd69cb481bfccc2a335ff4d2c68b197
albopilo <albopilosum2+12@gmail.com>
2025-09-18 14:23:22 +0700

    Update loyaltyFirebase.js

--- Hotel Software/millennium-app/src/loyaltyFirebase.js
+++ Hotel Software/millennium-app/src/loyaltyFirebase.js
@@ -1,17 +1,15 @@
-// loyaltyFirebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const loyaltyConfig = {
-  apiKey: "AIzaSyDNvgS_PqEHU3llqHt0XHN30jJgiQWLkdc",
-  authDomain: "e-loyalty-12563.firebaseapp.com",
-  projectId: "e-loyalty-12563",
-  storageBucket: "e-loyalty-12563.appspot.com",
-  messagingSenderId: "3887061029",
-  appId: "1:3887061029:web:f9c238731d7e6dd5fb47cc",
-  measurementId: "G-966P8W06W2"
+  apiKey: process.env.REACT_APP_LOYALTY_API_KEY,
+  authDomain: process.env.REACT_APP_LOYALTY_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_LOYALTY_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_LOYALTY_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_LOYALTY_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_LOYALTY_APP_ID,
+  measurementId: process.env.REACT_APP_LOYALTY_MEASUREMENT_ID,
 };
 
-// Initialise as a named secondary app
 const loyaltyApp = initializeApp(loyaltyConfig, "loyaltyApp");
 export const loyaltyDb = getFirestore(loyaltyApp);
